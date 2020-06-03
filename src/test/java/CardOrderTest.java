cc80a9c809ac5feb5c0e24700d36c9740c4a3f57
Dementev Slava <kursant344@gmail.com>
2020-06-03 14:51:40 +0300

    add CardOrderTest

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -1,2 +1,20 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+
 public class CardOrderTest {
+
+    @Test
+    void shouldSubmitRequest() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[class='form form_size_m form_theme_alfa-on-white']");
+        form.$("[data-test-id=name] input").setValue("Петр Иванов");
+        form.$("[data-test-id=phone] input").setValue("+79101234567");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+        $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
 }
