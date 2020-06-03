2275b772167ce55310d5c7da1d5563107a1a5027
Dementev Slava <kursant344@gmail.com>
2020-06-03 15:54:08 +0300

    add negative test

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -1,5 +1,6 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.*;
@@ -7,14 +8,59 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class CardOrderTest {
 
+    SelenideElement form = $("form");
+    SelenideElement name = form.$("[data-test-id=name] input");
+    SelenideElement phone = form.$("[data-test-id=phone] input");
+    SelenideElement agreement = form.$("[data-test-id=agreement]");
+    SelenideElement button = form.$("[type=button]");
+
+    @BeforeEach
+    void openHost() {
+        open("http://localhost:9999");
+    }
+
     @Test
     void shouldSubmitRequest() {
-        open("http://localhost:9999");
-        SelenideElement form = $("[class='form form_size_m form_theme_alfa-on-white']");
-        form.$("[data-test-id=name] input").setValue("Петр Иванов");
-        form.$("[data-test-id=phone] input").setValue("+79101234567");
-        form.$("[data-test-id=agreement]").click();
-        form.$("[type=button]").click();
+        name.setValue("Петр Иванов");
+        phone.setValue("+79101234567");
+        agreement.click();
+        button.click();
         $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void invalidNameTest(){
+        name.setValue("Petr Ivanov");
+        phone.setValue("+791198765432");
+        agreement.click();
+        button.click();
+        $("[data-test-id=name] span.input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void invalidPhoneTest(){
+        name.setValue("Петр Иванов");
+        phone.setValue("+7910123456");
+        agreement.click();
+        button.click();
+        $("[data-test-id=phone] span.input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void NoNameTest(){
+
+        phone.setValue("+7910123456");
+        agreement.click();
+        button.click();
+        $("[data-test-id=name] span.input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void NoPhoneTest(){
+        name.setValue("Петр Иванов");
+
+        agreement.click();
+        button.click();
+        $("[data-test-id=phone] span.input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
     }
 }
